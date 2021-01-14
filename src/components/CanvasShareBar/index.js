e54982e5521801d612bd187b370c7c3de73205ca
Areeba Aziz <aziza11@mcmaster.ca>
2021-01-13 22:56:59 -0500

    added shareable icons with tooltips

--- src/components/CanvasShareBar/index.js
+++ src/components/CanvasShareBar/index.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Facebook, Instagram, Twitter, Link } from '@material-ui/icons';
+import Tooltip from '@material-ui/core/Tooltip';
+
+const useStyles = makeStyles(theme => ({
+  shareIcon: {
+    cursor: 'pointer'
+  }
+}));
 
 export default function CanvasShareBar() {
+  const classes = useStyles();
+
   return (
-    <p>[Placeholder for share links]</p>
+    <div style={{
+      textAlign: 'right',
+      paddingRight: '1.8em'
+    }}>
+      Share on social media:
+      <div>
+        <Tooltip title="Twitter">
+          <Twitter className={classes.shareIcon} />
+        </Tooltip>
+          <Tooltip title="Facebook">
+          <Facebook className={classes.shareIcon} />
+        </Tooltip>
+        <Tooltip title="Instagram">
+          <Instagram className={classes.shareIcon} />
+        </Tooltip>
+        <Tooltip title="Copy link">
+          <Link className={classes.shareIcon} />
+        </Tooltip>
+      </div>
+    </div>
   );
 }
