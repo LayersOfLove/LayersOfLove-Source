2c2fd083fdb740e1ae9f8502a9ba9d4e612f964f
Areeba Aziz <aziza11@mcmaster.ca>
2021-01-15 22:52:32 -0500

    created Facebook and Twitter share popups using the react-share library

--- src/components/CanvasShareBar/index.js
+++ src/components/CanvasShareBar/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import { Facebook, Instagram, Twitter, Link } from '@material-ui/icons';
-import Tooltip from '@material-ui/core/Tooltip';
-import Grid from '@material-ui/core/Grid';
-import { Button, IconButton } from '@material-ui/core';
+import { 
+  Button,
+  IconButton, 
+  Grid,
+  Tooltip,
+  makeStyles
+} from '@material-ui/core';
+import {
+  FacebookShareButton,
+  TwitterShareButton
+} from 'react-share';
 
 const useStyles = makeStyles(theme => ({
   shareIcon: {
     cursor: 'pointer'
   }
 }));
+
+const BASE_URL = "https://layersoflove.ca"
 
 export default function CanvasShareBar() {
   const classes = useStyles();
@@ -31,25 +40,44 @@ export default function CanvasShareBar() {
           Submit
         </Button>
       </Grid>
+      
+      {/* Twitter */}
       <Grid item md={5}>
-        <IconButton color="secondary">
-          <Tooltip title="Twitter">
-            <Twitter className={classes.shareIcon} />
-          </Tooltip>
-        </IconButton>
+        <TwitterShareButton 
+          url={BASE_URL}
+          title="I just made a #LayersofLabourofLove digital collage with @_khadija_a_ and @laurakaykeeling! #DesignTO21"
+          related={["_khadija_a_", "laurakaykeeling"]}
+        >
+          <IconButton color="secondary">
+            <Tooltip title="Twitter">
+              <Twitter className={classes.shareIcon} />
+            </Tooltip>
+          </IconButton>
+        </TwitterShareButton>
 
-        <IconButton color="secondary">
-          <Tooltip title="Facebook">
-            <Facebook className={classes.shareIcon} />
-          </Tooltip>
-        </IconButton>
+      {/* Facebook */}
+        <FacebookShareButton 
+          url={BASE_URL}
+          quote="I just made a #LayersofLabourofLove digital collage with Khadija Aziz and Laura Keeling! #DesignTO21"   
+          hashtag="#LayersofLabourofLove"     
+        >
+          <IconButton color="secondary">
+            <Tooltip title="Facebook">
+              <Facebook 
+                className={classes.shareIcon} 
+              />
+            </Tooltip>
+          </IconButton>
+        </FacebookShareButton>
 
+      {/* Instagram */}
         <IconButton color="secondary">
           <Tooltip title="Instagram">
             <Instagram className={classes.shareIcon} />
           </Tooltip>
         </IconButton>
 
+      {/* Copy link */}
         <IconButton color="secondary">
           <Tooltip title="Copy link">
             <Link className={classes.shareIcon} />
@@ -57,26 +85,5 @@ export default function CanvasShareBar() {
         </IconButton>  
         </Grid>
     </Grid>
-    // <div style={{
-    //   position: 'relative'
-    // }}>
-    //   <div style={{
-    //     textAlign: 'left',
-    //     paddingTop: '0.4em'
-    //   }}>
-    //     <Tooltip title="Twitter">
-    //       <Twitter className={classes.shareIcon} />
-    //     </Tooltip>
-    //       <Tooltip title="Facebook">
-    //       <Facebook className={classes.shareIcon} />
-    //     </Tooltip>
-    //     <Tooltip title="Instagram">
-    //       <Instagram className={classes.shareIcon} />
-    //     </Tooltip>
-    //     <Tooltip title="Copy link">
-    //       <Link className={classes.shareIcon} />
-    //     </Tooltip>
-    //   </div>
-    // </div>
   );
 }
